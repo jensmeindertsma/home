0e92ce3ffc932560f36b7a00ba7bed22ed2496d7
Jens Meindertsma <mail@jensmeindertsma.com>
2025-06-09 15:55:47 +0200

    I guess this works for now

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,30 +4,32 @@ import { readdir, readFile } from "node:fs/promises";
 
 export default function Home() {
   return (
-    <>
-      <img src="/me.jpg" />
-      <h1 className="mt-6 font-mono text-5xl font-bold">
-        <span className="mb-2 block">Jens</span>
-        <span>Meindertsma</span>
-      </h1>
+    <div className="sm:m-auto sm:mt-20 sm:w-80 md:m-auto md:flex md:w-auto md:flex-row md:justify-center md:p-10">
+      <img src="/me.jpg" className="sm:mb-10 md:m-0 md:mr-20 md:w-75" />
+      <div className="m-auto md:m-0">
+        <h1 className="mt-6 font-mono text-5xl font-bold sm:mb-10">
+          <span className="mb-2 block">Jens</span>
+          <span>Meindertsma</span>
+        </h1>
 
-      <a
-        href="https://github.com/jensmeindertsma"
-        className="mt-6 flex h-24 flex-row justify-center p-4 font-mono text-xl font-bold"
-      >
-        <picture className="w-16">
-          <source
-            srcSet="/github-light.png"
-            media="(prefers-color-scheme: dark)"
-          />
-          <img src="/github-dark.png" className="block h-16" />
-        </picture>
-        <span className="ml-8 flex h-full items-center">{"=>"}</span>
-        <span className="ml-8 flex h-full items-center underline decoration-2">
-          {" "}
-          ABOUT ME
-        </span>
-      </a>
+        <a
+          href="https://github.com/jensmeindertsma"
+          className="font-boldmd:p-0 mt-6 flex h-24 flex-row justify-start p-4 font-mono text-xl"
+        >
+          <picture className="h-16 w-16">
+            <source
+              srcSet="/github-light.png"
+              media="(prefers-color-scheme: dark)"
+            />
+            <img src="/github-dark.png" className="block h-16 w-16" />
+          </picture>
+          <span className="ml-8 flex h-full items-center">{"=>"}</span>
+          <span className="ml-8 flex h-full items-center underline decoration-2">
+            {" "}
+            ABOUT ME
+          </span>
+        </a>
+      </div>
 
       {/*
       <ul>
@@ -41,7 +43,7 @@ export default function Home() {
         ))}
       </ul>
        */}
-    </>
+    </div>
   );
 }
 
